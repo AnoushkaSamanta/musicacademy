58c98ee91cf6ea98db8aac3a0a9a942047651556
AnoushkaSamanta <anoushka1701@gmail.com>
2025-03-18 23:41:22 +0530

    Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,14 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="relative w-full flex items-center justify-center">
         <Navbar/>
         </div>
-       
+        
         {children}
       </body>
     </html>
